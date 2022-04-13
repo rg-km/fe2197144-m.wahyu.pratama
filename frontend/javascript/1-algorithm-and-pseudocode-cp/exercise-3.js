5ec38b89f4e94ddbf0fa5a52fd7594cb75b0daa2
uyhaw18 <mwahyupratama18@gmail.com>
2022-04-13 10:36:25 +0700

    new

--- frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-3.js
+++ frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-3.js
@@ -1,3 +1,12 @@
 // Buatlah pseudocode untuk menjumlahkan bilangan genap yang ada di antara 1 sampai 10
 
 // TODO: answer here
+let j = 0;
+for(i = 1; i >= 10; i--){
+    if(i % 2 === 0){
+        j += i; 
+        console.log(i);
+    }
+    
+}
+console.log(i);
--- frontend/javascript/2-variable-cp/script.js
+++ frontend/javascript/2-variable-cp/script.js
@@ -1,9 +1,12 @@
 // Menukar variabel a dan b dengan operator matematika
-
+const prompt = require('prompt-sync')();
 let a = prompt('Masukan variabel pertama: ');
 let b = prompt('Masukan variabel kedua: ');
 
 // TODO: answer here
+let temp = a;
 
+a = b;
+b = temp;
 console.log(`Nilai a setelah ditukar: ${a}`);
 console.log(`Nilai b setelah ditukar: ${b}`);
